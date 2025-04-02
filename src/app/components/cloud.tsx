cb85a7345892088f829223c063caf009e7c63782
连颜少 <lianyanshao@gmail.com>
2025-04-02 09:36:28 +0800

    setting gh-page

--- src/app/components/cloud.tsx
+++ src/app/components/cloud.tsx
@@ -24,20 +24,7 @@ export default function App() {
 function Sky() {
   const ref = useRef<any>(null)
   const cloud0 = useRef<any>(null)
-//   const { color, x, y, z, range, ...config } = useControls({
-//     seed: { value: 1, min: 1, max: 100, step: 1 },
-//     segments: { value: 20, min: 1, max: 80, step: 1 },
-//     volume: { value: 6, min: 0, max: 100, step: 0.1 },
-//     opacity: { value: 0.8, min: 0, max: 1, step: 0.01 },
-//     fade: { value: 10, min: 0, max: 400, step: 1 },
-//     growth: { value: 4, min: 0, max: 20, step: 1 },
-//     speed: { value: 0.1, min: 0, max: 1, step: 0.01 },
-//     x: { value: 6, min: 0, max: 100, step: 1 },
-//     y: { value: 1, min: 0, max: 100, step: 1 },
-//     z: { value: 1, min: 0, max: 100, step: 1 },
-//     color: "white",
-//   })
-  const { color, x, y, z, range, ...config } = {
+  const { color, x, y, z, ...config } = useControls({
     seed: { value: 1, min: 1, max: 100, step: 1 },
     segments: { value: 20, min: 1, max: 80, step: 1 },
     volume: { value: 6, min: 0, max: 100, step: 0.1 },
@@ -49,7 +36,20 @@ function Sky() {
     y: { value: 1, min: 0, max: 100, step: 1 },
     z: { value: 1, min: 0, max: 100, step: 1 },
     color: "white",
-  }
+  })
+//   const { color, x, y, z, ...config } = {
+//     seed: { value: 1, min: 1, max: 100, step: 1 },
+//     segments: { value: 20, min: 1, max: 80, step: 1 },
+//     volume: { value: 6, min: 0, max: 100, step: 0.1 },
+//     opacity: { value: 0.8, min: 0, max: 1, step: 0.01 },
+//     fade: { value: 10, min: 0, max: 400, step: 1 },
+//     growth: { value: 4, min: 0, max: 20, step: 1 },
+//     speed: { value: 0.1, min: 0, max: 1, step: 0.01 },
+//     x: { value: 6, min: 0, max: 100, step: 1 },
+//     y: { value: 1, min: 0, max: 100, step: 1 },
+//     z: { value: 1, min: 0, max: 100, step: 1 },
+//     color: "white",
+//   }
   
   useFrame((state, delta) => {
     ref.current.rotation.y = Math.cos(state.clock.elapsedTime / 2) / 2
@@ -60,7 +60,7 @@ function Sky() {
     <>
       <SkyImpl />
       <group ref={ref}>
-        <Clouds material={THREE.MeshLambertMaterial} limit={400} range={range}>
+        <Clouds material={THREE.MeshLambertMaterial} limit={400}>
           <Cloud ref={cloud0} {...config} bounds={[x, y, z]} color={color} />
           <Cloud {...config} bounds={[x, y, z]} color="#eed0d0" seed={2} position={[15, 0, 0]} />
           <Cloud {...config} bounds={[x, y, z]} color="#d0e0d0" seed={3} position={[-15, 0, 0]} />
